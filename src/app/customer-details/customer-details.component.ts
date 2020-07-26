3ff6a211033ec0d91a73527ce33cc231959fb84a
Tarek Ibrahim <tarek.ibrahim@ecomz.com>
2020-07-26 16:20:55 +0300

    fixes

--- src/app/customer-details/customer-details.component.ts
+++ src/app/customer-details/customer-details.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CustomerService } from '../services/customer.service';
 import { Customer } from '../Models/Customer';
+import { Invoice } from '../Models/Invoice';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { InvoiceService } from '../services/invoice.service';
 
 @Component({
   selector: 'app-customer-details',
@@ -12,19 +15,40 @@ export class CustomerDetailsComponent implements OnInit {
 
   customerId: any;
   customer: Customer;
-  constructor(private customerService: CustomerService, private route: ActivatedRoute) { }
+  invoice: Invoice;
+  invoiceForm: FormGroup;
+  constructor(private customerService: CustomerService,
+              private route: ActivatedRoute,
+              private fb: FormBuilder,
+              private invoiceService: InvoiceService) { }
 
   ngOnInit() {
     // tslint:disable-next-line: radix
     let id = parseInt(this.route.snapshot.paramMap.get('id'));
     this.customerId = id;
     this.getCustomer();
+    this.createInvoice();
+  }
+
+  createInvoice() {
+    this.invoiceForm = this.fb.group({
+      invoiceNumber: [''],
+      total: [''],
+    });
   }
 
   getCustomer() {
     this.customerService.getCustomer(this.customerId)
-    .subscribe((data: Customer) => {this.customer = data; },
+      .subscribe((data: Customer) => { this.customer = data; },
     );
+  }
+
+  addInvoiceForCustomer() {
+    if (this.invoiceForm.valid) {
+      this.invoice = Object.assign({}, this.invoiceForm.value);
+      this.invoice.customerId = this.customerId;
+      this.invoiceService.addInvoice(this.invoice).subscribe(() => { console.log('Success'); });
+    }
   }
 
 }
